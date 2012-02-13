736073b3c18d6a54c2e56f8d4224d2cec1c82c7a
Hiroki Kouchi <hiroking0607@gmail.com>
2012-02-14 02:21:36 +0900

    add ViewPagerIndicator SampleView.

--- LeavingRecord/gen/com/niyaty/leavingrecord/R.java
+++ LeavingRecord/gen/com/niyaty/leavingrecord/R.java
@@ -298,8 +298,15 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int vpi__tab_unselected_pressed_holo=0x7f020007;
     }
     public static final class id {
+        public static final int add_page=0x7f04000a;
         public static final int horizontal=0x7f040000;
+        public static final int indicator=0x7f040007;
         public static final int none=0x7f040002;
+        public static final int pager=0x7f040008;
+        public static final int random=0x7f040009;
+        public static final int remove_page=0x7f04000b;
+        public static final int textView1=0x7f040005;
+        public static final int textView2=0x7f040006;
         public static final int triangle=0x7f040003;
         public static final int underline=0x7f040004;
         public static final int vertical=0x7f040001;
@@ -309,9 +316,14 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int default_title_indicator_footer_indicator_style=0x7f070001;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
-        public static final int record_list_view=0x7f030001;
-        public static final int vpi__tab=0x7f030002;
+        public static final int layout01=0x7f030000;
+        public static final int layout02=0x7f030001;
+        public static final int main=0x7f030002;
+        public static final int record_list_view=0x7f030003;
+        public static final int vpi__tab=0x7f030004;
+    }
+    public static final class menu {
+        public static final int menu=0x7f0b0000;
     }
     public static final class string {
         public static final int app_name=0x7f0a0001;
