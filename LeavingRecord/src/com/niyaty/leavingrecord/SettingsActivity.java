b70cacc1ccf7878cbdb38c056d0121596953596a
Hiroki Kouchi <hiroking0607@gmail.com>
2012-04-15 01:07:57 +0900

    mod SettingsActivity

--- LeavingRecord/src/com/niyaty/leavingrecord/SettingsActivity.java
+++ LeavingRecord/src/com/niyaty/leavingrecord/SettingsActivity.java
@@ -5,10 +5,13 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
 
 public class SettingsActivity extends PreferenceActivity {
 
-    Preference preference;
+    Preference arrivalPreference;
+    Preference leavingPreference;
+    Preference resttimePreference;
 
     public String ARRIVAL_KEY = "settingsArrivalPreference";
     public String LEAVING_KEY = "settingsLeavingPreference";
@@ -17,30 +20,40 @@ public class SettingsActivity extends PreferenceActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        addPreferencesFromResource(R.xml.pref);
-//        String[] searchbooksKeys = getResources().getStringArray(R.array.search_books);
-//        String[] searchbooksValues = getResources().getStringArray(R.array.search_books_value);
-        preference = findPreference(ARRIVAL_KEY);
+        addPreferencesFromResource(R.xml.pref);
+        
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        
+        arrivalPreference = findPreference(ARRIVAL_KEY);
+        arrivalPreference.setSummary(pref.getString(ARRIVAL_KEY, "09:00"));
+        leavingPreference = findPreference(LEAVING_KEY);
+        leavingPreference.setSummary(pref.getString(LEAVING_KEY, "18:00"));
+        resttimePreference = findPreference(RESTTIME_KEY);
+        resttimePreference.setSummary(pref.getString(RESTTIME_KEY, "01:00"));
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-//        getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(listener);
+        getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(listener);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-//        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(listener);
+        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(listener);
     }
 
     private SharedPreferences.OnSharedPreferenceChangeListener listener = new SharedPreferences.OnSharedPreferenceChangeListener() {
         @Override
         public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-//            if(key.equals()) {
-//                searchbooksPref.setSummary(searchbooksMap.get(sharedPreferences.getString(key, "Books")));
-//            }
+            if(key.equals(ARRIVAL_KEY)) {
+                arrivalPreference.setSummary(sharedPreferences.getString(ARRIVAL_KEY, ""));
+            } else if (key.equals(LEAVING_KEY)) {
+                leavingPreference.setSummary(sharedPreferences.getString(LEAVING_KEY, ""));
+            } else if (key.equals(RESTTIME_KEY)) {
+                resttimePreference.setSummary(sharedPreferences.getString(RESTTIME_KEY, ""));
+            }
         }
     };
 
